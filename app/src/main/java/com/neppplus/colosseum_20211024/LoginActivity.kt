558d69383237781f024dfc409a1659b76003f7f1
박수정 <zxzx4342@naver.com>
2021-10-31 14:04:26 +0900

    메인화면 -> LoginActivity로 이름 변경

--- app/src/main/java/com/neppplus/colosseum_20211024/LoginActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/LoginActivity.kt
@@ -1,23 +1,21 @@
 package com.neppplus.colosseum_20211024
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import com.neppplus.colosseum_20211024.databinding.ActivityMainBinding
+import com.neppplus.colosseum_20211024.databinding.ActivityLoginBinding
 import com.neppplus.colosseum_20211024.utils.ServerUtil
 import org.json.JSONObject
-import kotlin.math.log
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         setupEvents()
         setValues()
 
