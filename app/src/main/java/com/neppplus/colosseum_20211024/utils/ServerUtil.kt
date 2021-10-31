d6524cb74ef7c7b5d9c1294a70292486d33ffff7
박수정 <zxzx4342@naver.com>
2021-10-31 11:41:24 +0900

    회원가입 API 호출 함수 구현  - ServerUtil 기능 추가

--- app/src/main/java/com/neppplus/colosseum_20211024/utils/ServerUtil.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/utils/ServerUtil.kt
@@ -81,6 +81,47 @@ class ServerUtil {
 
         }
 
+
+//        회원가입 기능
+        fun putRequestSignUp(email: String, pw: String, nickname: String, handler: JsonResponseHandler?) {
+
+            val urlString = "${BASE_URL}/user"
+
+            val formData = FormBody.Builder()
+//                    name: "API서버 이름", 현재 function의 변수
+                .add("email", email)
+                .add("password", pw)
+                .add("nick_name", nickname)
+                .build()
+
+            val request = Request.Builder()
+                .url(urlString)
+                .put(formData)
+                .build()
+
+            val client = OkHttpClient()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+                    val bodyString = response.body!!.string()
+                    val jsonObj = JSONObject(bodyString)
+                    Log.d("서버응답본문", jsonObj.toString())
+                    handler?.onResponse(jsonObj)
+
+                }
+
+            })
+
+        }
+
+
     }
 
 }
