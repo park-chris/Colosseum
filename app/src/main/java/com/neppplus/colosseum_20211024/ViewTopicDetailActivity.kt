aa84882884834803426d83ddbb09713fdfd7e59d
박수정 <zxzx4342@naver.com>
2021-11-07 14:35:21 +0900

    토론 상세 -> 댓글 목록 파싱  - 댓글목록 ArrayList에 추가

--- app/src/main/java/com/neppplus/colosseum_20211024/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/ViewTopicDetailActivity.kt
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
+import com.neppplus.colosseum_20211024.adapters.ReplyAdapter
 import com.neppplus.colosseum_20211024.databinding.ActivityViewTopicDetailBinding
+import com.neppplus.colosseum_20211024.datas.ReplyData
 import com.neppplus.colosseum_20211024.datas.TopicData
 import com.neppplus.colosseum_20211024.utils.ServerUtil
 import org.json.JSONObject
+import java.util.ArrayList
 
 class ViewTopicDetailActivity : BaseActivity() {
 
     lateinit var binding: ActivityViewTopicDetailBinding
 
     lateinit var mTopicData : TopicData
+
+    val mReplyList = ArrayList<ReplyData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +47,30 @@ class ViewTopicDetailActivity : BaseActivity() {
     fun getTopicDatailFromServer() {
         ServerUtil.getRequestTopicDetail(mContext, mTopicData.id, "NEW", object : ServerUtil.JsonResponseHandler {
             override fun onResponse(jsonObj: JSONObject) {
+
+
+//                댓글 목록 JSONArray -> 파싱 -> mReplyList의 자료로 추가
+                val dataObj = jsonObj.getJSONObject("data")
+                val topicObj = dataObj.getJSONObject("topic")
+
+                val repliesArr = topicObj.getJSONArray("replies")
+
+                for ( i in 0 until repliesArr.length()) {
+
+                    val replyObj = repliesArr.getJSONObject(i)
+
+//                    JSONObject -> Replydata 객체로 변환.
+                    val replyData = ReplyData()
+                    replyData.id = replyObj.getInt("id")
+                    replyData.content = replyObj.getString("content")
+
+//                    댓글목록으로 추가
+                    mReplyList.add(replyData)
+
+                }
+
+//                리스트뷰의  목록에 변경 => 어댑터 새로고침
+
             }
 
         })
