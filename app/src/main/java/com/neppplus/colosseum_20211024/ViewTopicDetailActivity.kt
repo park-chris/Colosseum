e4861f7df8a686cfbdc64e4cea4ea4318b0b1e20
박수정 <zxzx4342@naver.com>
2021-11-07 13:00:19 +0900

    토픽 디테일 뷰 오나성

---
+++ app/src/main/java/com/neppplus/colosseum_20211024/ViewTopicDetailActivity.kt
@@ -0,0 +1,38 @@
+package com.neppplus.colosseum_20211024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
+import com.neppplus.colosseum_20211024.databinding.ActivityViewTopicDetailBinding
+import com.neppplus.colosseum_20211024.datas.TopicData
+
+class ViewTopicDetailActivity : BaseActivity() {
+
+    lateinit var binding: ActivityViewTopicDetailBinding
+
+    lateinit var mTopicData : TopicData
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_view_topic_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+        mTopicData = intent.getSerializableExtra("topic") as TopicData
+
+        binding.topicTitleTxt.text = mTopicData.title
+
+        Glide.with(mContext).load(mTopicData.imageURL).into(binding.topicImg)
+
+    }
+
+
+}
