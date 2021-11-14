992e0c9bdefdd6fd150b047fce5603b7d267b7c3
박수정 <zxzx4342@naver.com>
2021-11-14 13:51:47 +0900

    모든 화면의 onCreate에서  - 액션바가 있는지 확인 (?.let { })
     - 액션바 커스터마이징 실행 처리

--- app/src/main/java/com/neppplus/colosseum_20211024/BaseActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/BaseActivity.kt
@@ -1,5 +1,7 @@
 package com.neppplus.colosseum_20211024
 
+import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -10,10 +12,13 @@ abstract class BaseActivity : AppCompatActivity() {
 
     val mContext = this
 
+//    onCreate의 내용을 수정해서 (재정의해서) 사용 -> override
+
+
     abstract fun setupEvents()
     abstract fun setValues()
 
-    fun setCusomActionBar() {
+    fun setCustomActionBar() {
 
 //        일반 함수를 물려준다. -> 그 실행 내용까지 같이 내려줌.
 //        자식 (다른 화면) 에서는 -> 이 함수를 실행만 하면 바로 사용 가능.
@@ -27,6 +32,20 @@ abstract class BaseActivity : AppCompatActivity() {
         toolbar.setContentInsetsAbsolute(0,0)
 
 
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        여기에 적는 코드? -> 자식 (다른 화면)들의 super.oncreate => 실행되는 내용
+
+//        모든 화면이 만들어질때 -> 액션바가 있다면 -> 액션바 커스터마이징 기능 실행
+
+//        ActionBar가 있을때만 함수를 실행시켜주세요. let 문법 사용
+        supportActionBar?.let {
+            setCustomActionBar()
+        }
 
     }
 
