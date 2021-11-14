02c3dd054a26c0def3c4ab362ec9e3cecb9a7c2b
박수정 <zxzx4342@naver.com>
2021-11-14 13:40:31 +0900

    액션바 커스터마이징 기능 추가  - BaseActivity에 추가해서 모든 화면이 물려받게

--- app/src/main/java/com/neppplus/colosseum_20211024/BaseActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/BaseActivity.kt
@@ -1,6 +1,8 @@
 package com.neppplus.colosseum_20211024
 
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 
 // kotlin은 class를 만들면 final class로 만들어지기 때문에 상속해줄수없음
@@ -11,5 +13,21 @@ abstract class BaseActivity : AppCompatActivity() {
     abstract fun setupEvents()
     abstract fun setValues()
 
+    fun setCusomActionBar() {
+
+//        일반 함수를 물려준다. -> 그 실행 내용까지 같이 내려줌.
+//        자식 (다른 화면) 에서는 -> 이 함수를 실행만 하면 바로 사용 가능.
+
+        val defActionBar = supportActionBar!!
+        defActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+
+        defActionBar.setCustomView(R.layout.my_custom_action_bar)
+
+        val toolbar = defActionBar.customView.parent as Toolbar
+        toolbar.setContentInsetsAbsolute(0,0)
+
+
+
+    }
 
 }
